abc971d48ab7837339bf374e7517958bdb5aa11e
Dunn <dunnev941@gmail.com>
2018-11-29 00:03:15 -0500

    new

---
+++ kebab.js
@@ -0,0 +1,9 @@
+// var cont = document.getElementById('container');
+// cont.onmousemove = applyCustomPointer;
+
+// var customPointer = document.getElementByTagName('customPointer');
+
+// function applyCustomPointer(event) {
+//     customPointer.style.left = event.clientX + 'px';
+//     customPointer.style.top = event.clientY + 'px';
+// }
